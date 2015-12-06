94b88321e93135669cd849edb3b78a579e36b8cb
Bijoy Varghese <bijoy.varghese.1990@gmail.com>
2015-12-06 18:26:27 +0000

    validations added

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,10 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
-	validates :name, :presence => true
+	validates :name, :presence => true, :length => { :minimum => 4 }
+	validates :address, :presence => true
+	validates :description, :presence => true
 
-	
+
 	paginates_per 5
 	max_paginates_per 20
 
